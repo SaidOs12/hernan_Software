c9f5a4152d7c8fe9bb3431112cdfee8a37630588
Deisler Said Prado Ospina <90699582+SaidOs12@users.noreply.github.com>
2024-10-05 22:50:44 -0500

    feat:delete changes

--- src/routes/login.js
+++ src/routes/login.js
@@ -19,7 +19,6 @@ router.post('/login', async (req,res) =>{
       
       
       req.flash('success', 'Bienvenido usuario: ' +  busqueda[0].cedula)
-      sessionStorage.setItem('userLogged', data[0]);
       if (busqueda[0].tipo_usuario === "admin") {
         res.redirect('/dashboard');
       } else if (busqueda[0].tipo_usuario === "bibliotecario") {
