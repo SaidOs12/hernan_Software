21df466bc5633e23cce86a4c220df5b32166b713
Deisler Said Prado Ospina <90699582+SaidOs12@users.noreply.github.com>
2024-10-06 09:38:50 -0500

    feat: Fixing

--- src/routes/register.js
+++ src/routes/register.js
@@ -8,7 +8,7 @@ router.get('/registro', (req, res) => {
   res.render('registro');
 });
 
-router.post('/registro', async (req, res) => {
+router.post('/infopersonal', async (req, res) => {
   try {
     console.log(req.body);
     const { error } = schemasPersona.validate(req.body);
