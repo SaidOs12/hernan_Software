d8246236fc8abbce21adcc522d196c8504fddec8
Deisler Said Prado Ospina <90699582+SaidOs12@users.noreply.github.com>
2024-10-07 00:26:16 -0500

    feat:Delete js

--- src/routes/usuario.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.get('/infopersonal', async (req,res) =>{
-  res.render('page-informacionpersonal');
-});
-
-module.exports = router;
