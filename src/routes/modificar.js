b49fcfdaaba0a206b0431a1b06e151b6fbf0d0df
Deisler Said Prado Ospina <90699582+SaidOs12@users.noreply.github.com>
2024-10-07 04:03:00 -0500

    feat:Update

--- src/routes/modificar.js
+++ src/routes/modificar.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db');
 const bcrypt = require('bcryptjs');
+const schemasPersona = require('../schemas/schemasUsuario');
 
 router.get('/page-modificacion', async (req, res) => {
   res.render('page-modificacion');
@@ -9,6 +10,13 @@ router.get('/page-modificacion', async (req, res) => {
 
 router.post('/page-modificacion', async (req, res) => {
   try {
+    console.log(req.body);
+    const { error } = schemasPersona.validate(req.body);
+    if (error) {
+      req.flash('error', error.details[0].message);
+      res.redirect('/page-modificacion');
+    }
+    
     const { cedula, nombre, apellidos, telefono, direccion } = req.body;
 
     // Update the user information
