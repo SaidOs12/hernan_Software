9be2000bb00732c104c639ba43ad953db7fe4fda
Deisler Said Prado Ospina <90699582+SaidOs12@users.noreply.github.com>
2024-10-07 01:22:02 -0500

    feat:Update

--- src/routes/proceso.js
+++ src/routes/proceso.js
@@ -5,5 +5,18 @@ const router = express.Router();
 router.get('/proceso', async (req,res) =>{
   res.render('proceso');
 });
-
+router.post('/proceso', async (req,res) =>{
+  try {
+    const { cedula } = req.body;
+    
+    // Si el usuario existe, lo eliminamos
+    await pool.query('DELETE FROM persona WHERE cedula = ?', [cedula]);
+    
+    req.flash('success', 'Usuario eliminado correctamente');
+    res.redirect('/page-bancopreguntas'); // O redirige a donde prefieras después de eliminar
+  } catch (error) {
+      console.error(error);
+      res.status(500).send("Error en la petición: " + error.message);
+  }  
+})
 module.exports = router;
