1435210c3c0f4b7096be79931dd1e6986080a5cd
Deisler Said Prado Ospina <90699582+SaidOs12@users.noreply.github.com>
2024-10-07 01:10:25 -0500

    feat:update

--- src/routes/evaluacion.js
+++ src/routes/evaluacion.js
@@ -23,53 +23,11 @@ router.post('/unidadUnoAgregar', async (req,res) =>{
             req.flash('error', 'No se encontro ningun usuario con esa cedula registrada')
             res.redirect('/unidadUnoAgregar');
         }
-    function obtenerRespuesta(indice) {
-            const opcion = [req.body.opciona, req.body.opcionb, req.body.opcionc, req.body.opciond];
-            const respuesta = opcion[indice - 1];
-            return respuesta;
-          }
-    
-    const {ievasoc, dificultad, tipoPregunta, enunciado, opciona, opcionb, opcionc, opciond, respuestaCorrecta, retroalimentacion } = req.body;
-    const preguntaValidacion = {
-     
-        dificultad, 
-        tipoPregunta,   
-        enunciado, 
-        opciona,
-        opcionb,
-        opcionc,
-        opciond,
-        respuestaCorrecta: obtenerRespuesta(req.body.respuestaCorrecta), 
-        retroalimentacion
     }
-    
-    const { error } = schemasPreguntas.validate(preguntaValidacion) ;
-    if (error) {
-        req.flash('error', error.details[0].message);
-        res.redirect('/unidadUnoAgregar');
-    }
-    else {
-
-        const nuevaPregunta = {
-            idEvaAsoc: 1,
-            dificultad, 
-            tipoPregunta, 
-            enunciado,  
-            opcion: opciona + ";" + opcionb + ";" + opcionc + ";" + opciond,
-            respuestaCorrecta: obtenerRespuesta(req.body.respuestaCorrecta), 
-            retroalimentacion
-        }
-    
-  
-
-    await pool.query('INSERT INTO pregunta set ?', [nuevaPregunta])
-    req.flash('success', 'Listado de Preguntas cargado exitosamente')
-    res.redirect('/bancoPreguntas1');
-}}
-catch(error){
-    console.error(error)
-    res.send("ERROR EN LA PETICION" + error)
-}    
+    catch(error){
+        console.error(error)
+        res.send("ERROR EN LA PETICION" + error)
+    }    
 })
 
 module.exports = router;
