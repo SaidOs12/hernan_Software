2e6fd0eb30535e4707fca23718aab969afd8f9da
Deisler Said Prado Ospina <90699582+SaidOs12@users.noreply.github.com>
2024-10-07 20:16:27 -0500

    Testing

--- src/routes/ejemplaresLista.js
+++ src/routes/ejemplaresLista.js
@@ -4,6 +4,7 @@ const pool = require('../db');
 
 router.get('/ejemplaresListas', async (req,res) =>{
   const usuario = await pool.query('SELECT * FROM ejemplar');
+  res.render('page-ejemplaresLista');
 });
 
 module.exports = router;  
