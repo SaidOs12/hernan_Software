960419c944e1990424afc3dbb4797cc4cbabb1c9
Deisler Said Prado Ospina <90699582+SaidOs12@users.noreply.github.com>
2024-10-23 04:14:51 -0500

    feat:Update

--- src/routes/registerPrestamo.js
+++ src/routes/registerPrestamo.js
@@ -16,57 +16,64 @@ router.post('/registerPrestamo', async (req, res) => {
     const existeIDInv = await pool.query('SELECT * FROM ejemplar WHERE idInventario = ?', [req.body.idInventario]);
     const existeID = await pool.query('SELECT * FROM persona WHERE cedula = ?', [req.body.cedula]);
     const limitePres = await pool.query('SELECT * FROM prestamo WHERE cedula = ?', [req.body.cedula]);
+    const diferenciaDias = Math.floor((fechaTope - fechaPrestamo) / (1000 * 60 * 60 * 24));
     if (existeID.length == 0) {
       req.flash('error', 'La cedula no esta registrada');
       res.redirect('/registerPrestamo');
       return;
-    }else{
-      if(existeIDInv.length === 0){
-        req.flash('error', 'La id del Ejemplar no esta registrado');
-        res.redirect('/registerPrestamo');
+    }
+    if(existeIDInv.length === 0){
+      req.flash('error', 'La id del Ejemplar no esta registrado');
+      res.redirect('/registerPrestamo');
+    return;
+    }
+    if(existeIDInv[0].estado !== "Disponible"){
+      req.flash('error', 'El ejemplar no se encuentra disponible');
+      res.redirect('/registerPrestamo');
       return;
-      }else {
-        if(existeIDInv[0].estado !== "Disponible"){
-          req.flash('error', 'El ejemplar no se encuentra disponible');
-          res.redirect('/registerPrestamo');
-          return;
-        }else{
-          if(existeID[0].multado){
-            req.flash('error', 'El usuario se encuentra multado');
-            res.redirect('/registerPrestamo');
-            return;
-          }else{
-            if((limitePres.length>=3 && existeID[0].tipo_usuario === "alumno") ||(limitePres.length>=5 && existeID[0].tipo_usuario === "profesor")){
-              req.flash('error', 'El usuario tiene demasiados prestamos activos');
-              res.redirect('/registerPrestamo');
-              return;
-            }else{
-              const {
-                idInventario,
-                cedula,
-                fecha,
-                fechaTope
-              } = req.body;
-              const prestamo = {
-                idInventario,
-                cedula,
-                fecha,
-                fechaTope
-              }
-              console.log("BIEN")
-              await pool.query('INSERT INTO prestamo SET ?', [prestamo]);
-              req.flash('success', 'Prestado registrado correctamente');
-              res.redirect('/registerPrestamo');
+    }
+    if(existeID[0].multado){
+      req.flash('error', 'El usuario se encuentra multado');
+      res.redirect('/registerPrestamo');
+      return;
+    }
+    if((limitePres.length>=3 && existeID[0].tipo_usuario === "alumno") ||(limitePres.length>=5 && existeID[0].tipo_usuario === "profesor")){
+      req.flash('error', 'El usuario tiene demasiados prestamos activos');
+      res.redirect('/registerPrestamo');
+      return;
+    }
+    if((ldiferenciaDias>7 && existeID[0].tipo_usuario === "alumno") ||(limitePres.length>14 && existeID[0].tipo_usuario === "profesor")){
+      req.flash('error', 'El lector excede el limite de fecha tope de devolución');
+      res.redirect('/registerPrestamo');
+      return;
+    }
+    const {
+      idInventario,
+      cedula,
+      fecha,
+      fechaTope
+    } = req.body;
+    const prestamo = {
+      idInventario,
+      cedula,
+      fecha,
+      fechaTope
+    }
+    console.log("BIEN")
+    await pool.query('INSERT INTO prestamo SET ?', [prestamo]);
+    await pool.query('UPDATE ejemplar SET estado = "Prestado" WHERE idInventario = ?', [req.body.idInventario]);
+    req.flash('success', 'Prestado registrado correctamente');
+    res.redirect('/registerPrestamo');
               
-            }
-          }
+            
+          
 
           
-        };
         
         
-      }
-    }
+        
+      
+    
   } catch (error) {
     console.log("MAL")
     res.status(400).send('Error FATAL: ' + error);
