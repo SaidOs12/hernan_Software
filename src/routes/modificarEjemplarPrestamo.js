afc326753fd0d8a92ad781b0b6b63143872f74c9
Deisler Said Prado Ospina <90699582+SaidOs12@users.noreply.github.com>
2024-10-24 11:17:58 -0500

    feat:Fix bug

--- src/routes/modificarEjemplarPrestamo.js
+++ src/routes/modificarEjemplarPrestamo.js
@@ -8,37 +8,42 @@ router.get('/page-modificacionEjemPrestamo', async (req, res) => {
 
 router.post('/page-modificacionEjemPrestamo', async (req, res) => {
   try {
-    const multado=false;
-    if(req.body.multa === "si"){
-      multado=true
-    }
-    const { idPrestamo, idInventario, cedula, fechaDevolucion} = req.body;
+    let multado = false;  // Cambiado de const a let
 
-    // Update the user information
-    await pool.query(
+if(req.body.multa === "si") {
+    multado = true;
+}
+
+const { idPrestamo, idInventario, cedula, fechaDevolucion } = req.body;
+
+// Update the user information
+await pool.query(
       'UPDATE prestamo SET idInventario = ?, cedula = ?, fechaDevolucion = ?, multa = ? WHERE idPrestamo = ?',
       [idInventario, cedula, fechaDevolucion, multado, idPrestamo]
-    );
-    await pool.query(
-      'UPDATE ejemplar SET estado = ? WHERE idInventario = ?',
-      ["Disponible", idInventario]
-    );
-  
-    if(multado){
-      const {
-        idPrestamo,
-        cedula
-      } = req.body;
-      const multa = {
-        idPrestamo,
-        cedula      
-      };
-      req.flash('success', 'Prestamo actualizado correctamente, registremos la multa');
-      res.redirect('/registerMulta',  { multa });
-    }else{
-    req.flash('success', 'Prestamo actualizado correctamente');
-    res.redirect('/ejemplaresListasPrestamo');
-  }
+      );
+
+      await pool.query(
+          'UPDATE ejemplar SET estado = ? WHERE idInventario = ?',
+          ["Disponible", idInventario]
+      );
+
+      if(multado) {
+          const {
+              idPrestamo,
+              cedula
+          } = req.body;
+          
+          const multa = {
+              idPrestamo,
+              cedula
+          };
+          
+          req.flash('success', 'Prestamo actualizado correctamente, registremos la multa');
+          res.redirect('/registerMulta', { multa });
+      } else {
+          req.flash('success', 'Prestamo actualizado correctamente');
+          res.redirect('/ejemplaresListasPrestamo');
+      }
   } catch (error) {
     console.error(error);
     res.status(500).send("Error en la petición: " + error.message);
